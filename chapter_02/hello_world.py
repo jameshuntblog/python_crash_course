2ee9369deedebafcae4b4ea27b5f7aa09f7d6e93
James Hunt <49603248+jameshuntblog@users.noreply.github.com>
2022-04-14 19:16:00 -0400

    added lines

--- chapter_02/hello_world.py
+++ chapter_02/hello_world.py
@@ -1,7 +1,10 @@
 # hello_world.py
 
-# message = "Hello Python world!"
-# print(message)
+message = "Hello Python world!"
+print(message)
 
 message = "Hello Python Crash Course world!"
+print(message)
+
+message = "Hello Python Crash Course reader!"
 print(message)
