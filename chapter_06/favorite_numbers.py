3fa7c3d8dff84c1398dee2679e91f776851cc874
James Hunt <49603248+jameshuntblog@users.noreply.github.com>
2022-05-16 20:18:13 -0400

    latest and greatest

---
+++ chapter_06/favorite_numbers.py
@@ -0,0 +1,8 @@
+# favorite_numbers.py
+
+favorite_numbers = {'James':9,'Duke':12,'John':29,'Kathleen':56,'Laser':16}
+print(f"James's favorite number is {favorite_numbers['James']}.")
+print(f"Duke's favorite number is {favorite_numbers['Duke']}.")
+print(f"John's favorite number is {favorite_numbers['John']}.")
+print(f"Kathleen's favorite number is {favorite_numbers['Kathleen']}.")
+print(f"Laser's favorite number is {favorite_numbers['Laser']}.")
