8765932b0c15e2e240222bd0b41b71bb69e64416
James Hunt <49603248+jameshuntblog@users.noreply.github.com>
2022-05-23 16:54:42 -0400

    latest and greatest

---
+++ chapter_08/cities.py
@@ -0,0 +1,8 @@
+# cities.py
+
+def describe_city(city_name,country="United States"):
+    print(f"{city_name} is in {country}.")
+
+describe_city("Chicago")
+describe_city("San Francisco")
+describe_city("Dublin","Ireland")
---
+++ chapter_08/favorite_book.py
@@ -0,0 +1,6 @@
+# favorite_book.py
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title.title()}.")
+
+favorite_book('the hobbit')
