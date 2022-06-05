ca3e4b4d2f94ddc9cbf9596e953f4b7dec515e90
James Hunt <49603248+jameshuntblog@users.noreply.github.com>
2022-06-04 21:14:39 -0400

    latest and greatest

---
+++ python_crash_course_2e/chapter_10/verify_user.py
@@ -0,0 +1,39 @@
+# verify_user.py
+
+import json
+
+def get_stored_username():
+    """Get stored username if available"""
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        yes_or_no = input(f"Is {username} the correct username? Enter 'y' for yes or 'n' "\
+            f"for no.")
+        if yes_or_no == 'y':
+            print(f"Welcome back, {username}!")
+        else:
+            username = get_new_username()
+            print(f"We'll remember you when you come back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
+
+greet_user()
